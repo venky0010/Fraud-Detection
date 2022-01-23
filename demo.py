43fa3967fb5deab99320c2b74bc78f06bb99d61e
Venkatesh Mohan Sharma <63727056+venky0010@users.noreply.github.com>
2022-01-24 01:18:29 +0530

    Update demo.py
    Added grouping python block

--- demo.py
+++ demo.py
@@ -142,3 +142,129 @@ def Algorithm4(IPMF, S1):
     
     #print("Algorithm 4",chi2, p, dof, ex)
     return p
+
+#Staging and computations to before calling algorithm functions
+def Computation(section, pmf, cdf, responses, answers):
+    
+    for i in section.index[:-1]:
+        for j in section.index[1:]:
+            
+            print(i, j)
+            student1 = section.loc[i, 'emis_username']
+            student2 = section.loc[j, 'emis_username']
+            print(student1, student2)
+            student1_sequence = responses[student1]
+            student2_sequence = responses[student2]
+            #print(student1_sequence)
+            #print(student2_sequence)
+            
+            #result_algo1 = Staging(student1_sequence, student2_sequence, pmf, cdf, answers)
+            x = method1_staging(student1_sequence, student2_sequence, answers, pmf, cdf)
+        break
+        
+        
+def method1_staging(response1, response2, correctAnswers, pmf, cdf):
+    
+    CPCA = []                  #Current paper's correct answers, saves True boolean value for right option
+    PMF  = []                  #Current paper's pmf
+    CPCA_inverse = []          #saves True boolean values for wrong options and False for correct option
+    PM   = []                  #Pair's match on answers, saves True boolean value for matching option **
+    PPM  = []                  #Pair's matching PMF **
+    inversePMF = []            #PMF for wrong answers
+    
+    bits = []
+    
+    for ques, ans1 in response1.items():
+        
+        if ques in response2:                          # if question match -> solve, else: skip
+            
+            x = [0]*5
+            x[correctAnswers[ques]-1] = 1              #if correct option in 2, then appends list [0, 1, 0, 0, 0]
+            CPCA.append(x)
+            CPCA_inverse.append([1-i for i in x])
+            PMF.append(list(pmf[ques].values())[1:])
+            
+            if ans1 == response2[ques]:                #check if both have same answers
+                
+                y = [0]*5
+                y[ans1-1] = 1
+                PM.append(y)                           # this y might not be same as x, depends if selected option is same as correct option
+                
+                if ans1 == correctAnswers[ques]:
+                    
+                    bits.append(0)
+                
+                else:
+                    
+                    bits.append(1)
+                
+            else:
+                
+                PM.append([0, 0, 0, 0, 1])
+                bits.append(1)
+                
+        
+                
+    PPM = [[PMF[j][i]*PM[j][i] for i in range(len(PM[j]))] for j in range(len(PMF))]
+    inversePMF = [[PMF[j][i]*(1-CPCA[j][i]) for i in range(len(CPCA[j]))] for j in range(len(PMF))]
+    
+    #print(len(PM), len(CPCA), len(CPCA_inverse), len(PPM), len(inversePMF), len(bits))
+    #print("\nPairs Match", PM[:5])
+    #print("\ncorrectAnswers", CPCA[:5])
+    #print("Bits", bits)
+    #print("\nWrong Answers", CPCA_inverse[:5])
+    #print("\nmatching probability", PPM[:5])
+    #print("\nwrong answer probability", inversePMF[:5])
+    
+    staging1 = [[bits[i]*PM[i][j] for j in range(len(PM[i]))] for i in range(len(PM))]                 #Stores bits for ignored answers
+    staging2 = [[PMF[j][i]*staging1[j][i] for i in range(len(staging1[j]))] for j in range(len(PMF))]  #PMF values for ignored answers
+    
+    #print("\nstaging1", staging1[:10])
+    #print("\nstaging2", staging2[:10])
+    #algo1 = Algorithm1(np.array(PMF), np.array(PM), np.array(PPM))
+    #algo2 = Algorithm2(np.array(PMF), np.array(PM), np.array(PPM))
+    #algo3 = Algorithm3(np.array(inversePMF), np.array(PPM), np.array(staging1), np.array(staging2))
+    algo4 = Algorithm4(np.array(inversePMF), np.array(staging1))
+    
+    
+#Final Block which runs staging and Algorithms by grouping sections across all schools and regions
+RESULTS = {}
+#Calculate Probability Distribution
+data = df1.copy()
+DISTRICTS = data.groupby(['district_name'])
+for district_name, item in DISTRICTS:
+
+    district = DISTRICTS.get_group(district_name)
+    BLOCKS = district.groupby(['block_name'])
+    
+    for block_name, value in BLOCKS:
+        
+        block = BLOCKS.get_group(block_name)
+        EDU_DISTS = block.groupby(['edu_dist_name'])
+        
+        for edu_name, value in EDU_DISTS:
+            
+            edu_dist = EDU_DISTS.get_group(edu_name)
+            SCHOOLS = edu_dist.groupby(['school_name'])
+            
+            for school_name, value in SCHOOLS:
+                
+                school = SCHOOLS.get_group(school_name)
+                SECTIONS = school.groupby(['Section'])
+                
+                for section_name, value in SECTIONS:
+                    
+                    print(district_name, block_name, edu_name, school_name, section_name)
+                    section = SECTIONS.get_group(section_name)
+                    print(len(section), len(set(section['emis_username'].tolist())))
+                    name = district_name+"_"+block_name+"_"+edu_name+"_"+school_name+"_"+section_name
+                    
+                    if name not in section_results:
+                        
+                        result = Computation(section, pmf, cdf, allResponsesWithStudentId, correctAnswers)
+                        RESULTS[name] = result
+                    break
+                break
+            break
+        break
+    break
